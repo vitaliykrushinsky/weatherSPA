cf4c96b4e6cbb919af0858bfd5ac479dc1f26d69
Vitaliy Krushinsky <vitaliykrushinsky@gmail.com>
2015-10-26 17:05:51 +0200

    Added API from http://openweathermap.org/api and set up route pages

--- app.js
+++ app.js
@@ -10,13 +10,47 @@ myApp.config(function($routeProvider) {
         templateUrl: 'pages/forecast.html',
         controller: 'forecastController'
     })
+    .when('/forecast/:days', {
+        templateUrl: 'pages/forecast.html',
+        controller: 'forecastController'
+    })
+});
+
+myApp.service('customService', function() {
+    this.cityName = 'Kiev';
 });
 
 
-myApp.controller('homeController', ['$scope', function($scope) {
+myApp.controller('homeController', ['$scope','customService', function($scope, customService) {
+    $scope.city = customService.cityName;
+    $scope.$watch('city', function() {
+        customService.cityName = $scope.city;
+    });
     
 }]);
 
-myApp.controller('forecastController', ['$scope', function($scope) {
+myApp.controller('forecastController', ['$scope','customService','$resource','$routeParams', function($scope, customService, $resource, $routeParams) {
+    
+    $scope.city = customService.cityName;
+    
+    $scope.days = $routeParams.days || 2;
+    console.log($scope.days)
+    
+    $scope.weatherAPI = $resource('http://api.openweathermap.org/data/2.5/forecast/daily?q=Kiev&lang=uk&units=metric&appid=bd82977b86bf27fb59a04b61b657fb6f', 
+        {
+        callback: 'JSON_CALLBACK'
+        },
+        {
+        get: { method: 'JSONP'}
+        }  
+    );
+    
+    $scope.weatherResult = $scope.weatherAPI.get({ q: $scope.city, cnt: $scope.days });
+    //console.log($scope.weatherResult);
+    
+    $scope.convertToDate = function(dt) {
+        return new Date(dt * 1000);
+    }
+    
     
 }]);
