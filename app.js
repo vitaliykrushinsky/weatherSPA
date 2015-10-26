47aa8079d09500633ce51575e3d2cb5132293f62
Vitaliy Krushinsky <vitaliykrushinsky@gmail.com>
2015-10-26 19:24:41 +0200

    Done basic functionality and little improve UI

--- app.js
+++ app.js
@@ -33,10 +33,11 @@ myApp.controller('forecastController', ['$scope','customService','$resource','$r
     
     $scope.city = customService.cityName;
     
-    $scope.days = $routeParams.days || 2;
-    console.log($scope.days)
+    $scope.days = $routeParams.days || '2';
+    console.log($scope.days);
     
-    $scope.weatherAPI = $resource('http://api.openweathermap.org/data/2.5/forecast/daily?q=Kiev&lang=uk&units=metric&appid=bd82977b86bf27fb59a04b61b657fb6f', 
+    
+    $scope.weatherAPI = $resource('http://api.openweathermap.org/data/2.5/forecast/daily?&lang=uk&units=metric&appid=bd82977b86bf27fb59a04b61b657fb6f', 
         {
         callback: 'JSON_CALLBACK'
         },
@@ -44,6 +45,7 @@ myApp.controller('forecastController', ['$scope','customService','$resource','$r
         get: { method: 'JSONP'}
         }  
     );
+    //console.log($scope.weatherAPI);
     
     $scope.weatherResult = $scope.weatherAPI.get({ q: $scope.city, cnt: $scope.days });
     //console.log($scope.weatherResult);
